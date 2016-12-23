b6eb346d99561f5132f5d8a23c887876004ed49d
Chris Li <kittokevin@gmail.com>
2016-12-23 12:05:49 -0800

    RDD Transformation

--- rdd/learn_rdd.py
+++ rdd/learn_rdd.py
@@ -5,8 +5,6 @@ import pref.setting as pref
 conf = SparkConf().setMaster('local').setAppName('My App')
 sc = SparkContext(conf=conf)
 
-
-
 #dir_path = os.path.dirname(os.path.realpath(__file__))
 #cwd = os.getcwd()
 
@@ -15,3 +13,16 @@ sc = SparkContext(conf=conf)
 
 data_dir = pref.get_env_variable('SPARK_DATA')
 lines = sc.textFile(data_dir + '/readme.dat')
+filines = lines.filter(lambda line: "guide" in line)
+#filines.first()
+print(filines.count())
+
+inputRDD = sc.textFile(data_dir + '/GitHubLog.txt')
+errorsRDD = inputRDD.filter(lambda line: 'ERROR' in line)
+warnsRDD = inputRDD.filter(lambda line: 'WARN' in line)
+badLinesRDD = errorsRDD.union(warnsRDD)
+
+print("Num of Errors: " + str(errorsRDD.count()))
+print("Num of Warns: " + str(warnsRDD.count()))
+print("Num of Bad Lines: " + str(badLinesRDD.count()))
+
