4372251e666cfecb9e31cb451c1b18550646c971
Chris Li <kittokevin@gmail.com>
2017-01-25 12:11:27 -0800

    PairRDD: combineByKey

--- rdd/common/pair_rdd.py
+++ rdd/common/pair_rdd.py
@@ -2,12 +2,20 @@ import operator
 
 
 def test_pair_rdd(lines_rdd):
-    words_rdd = lines_rdd.flatMap(lambda line: line.split())
+    words_rdd = get_words_rdd(lines_rdd)
     pair_rdd = words_rdd.map(lambda x: (x, 1))
     words_reduce = words_reduce_by_key(pair_rdd)
-    words_reduce02 = words_reduce_by_key(words_map_values(pair_rdd))
-    print_pair_rdd("Regular", words_reduce)
-    print_pair_rdd("Added 1", words_reduce02)
+    # words_reduce2 = words_reduce_by_key(words_map_values(pair_rdd))
+    # words_reduce3 = words_count_combine_by_key(pair_rdd)
+    words_reduce4 = word_list_combine_by_key(words_reduce.sortByKey())
+    # print_pair_rdd("Regular", words_reduce)
+    # print_pair_rdd("Added 1", words_reduce2)
+    # print_pair_rdd("combineByKey", words_reduce3)
+    print_pair_rdd("combineByKey", words_reduce4)
+
+
+def get_words_rdd(lines_rdd):
+    return lines_rdd.flatMap(lambda line: line.split())
 
 
 def words_reduce_by_key(words_rdd):
@@ -16,6 +24,26 @@ def words_reduce_by_key(words_rdd):
 
 def words_map_values(words_rdd):
     return words_rdd.mapValues(lambda x: x + 1)
+
+
+def words_count_combine_by_key(pair_rdd):
+    return pair_rdd.combineByKey(
+        lambda x: x,
+        lambda x, y: x + y,
+        lambda x, y: x + y)
+
+
+def word_list_combine_by_key(pair_rdd):
+    keys = pair_rdd.keys()
+    alpha_pair_rdd = keys.map(lambda x: (x[:1], x))
+
+    #alpha_pair_rdd.keys().foreach(print_elem)
+
+    return  alpha_pair_rdd.combineByKey(
+        lambda x: (x, 1),
+        lambda x, y: (x[0] + ", " + y, x[1] + 1),
+        lambda x, y: (x[0] + ", " + y[0], x[1] + y[1])
+    ).map(lambda z: (z[0], "[" + str(z[1][1]) + "] : " + z[1][0]))
 
 
 def print_pair_rdd(header, pair_rdd):
@@ -27,3 +55,6 @@ def print_pair_rdd(header, pair_rdd):
 
     for k, v in sorted_list:
         print "%s : %s" % (str(k), str(v))
+
+def print_elem (x):
+    print(x)
